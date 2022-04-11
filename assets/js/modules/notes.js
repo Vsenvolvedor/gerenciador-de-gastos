e2909d14ba8cd12e36238bbe3feccf5f2892570e
Vitor <catchtherainbow33@gmail.com>
2022-04-10 22:10:57 -0300

    adicionar anotaçoes completo

---
+++ assets/js/modules/notes.js
@@ -0,0 +1,69 @@
+import modal from "../script.js"
+
+export default class Notes {
+  constructor(wrapper,form){
+    this.wrapper = document.querySelector(wrapper)
+    this.form = document.querySelector(form)
+    this.inputs = [...this.form].filter((item) => item.id === 'text' || item.id === 'title')
+    this.btn = [...this.form].filter((item) => item.id === 'btn')[0]
+
+    this.addElement = this.addElement.bind(this)
+  }
+  createElement(){
+    const element = `
+    <div class="note">
+        <div>
+          <h3>
+            ${this.form.elements.title.value}
+          </h3>
+          <button class="delet-note">
+            <img src="assets/images/delete.svg" alt="Deletar">
+          </button>
+        </div>
+        <p>
+          ${this.form.elements.text.value}
+        </p>
+    </div>
+    `
+    return element
+  }
+  deletEvent(){
+    this.deletElements = document.querySelectorAll('.delet-note')
+    this.addEventClick()
+  }
+  deletElement({target}){
+    target.parentElement.parentElement.parentElement.remove()
+  }
+  addElement(){
+    const [input1,input2] = this.inputs.map((input) => {return !(input.value === '')});
+    if(input1 && input2){
+      const element = this.createElement();
+      this.wrapper.innerHTML += element;
+      modal.toggleMenu()
+      this.deletEvent()
+      this.inputs.forEach((input) => {
+        input.value = ''    
+      })
+    } else {
+      this.inputs.forEach((input) => {
+        if(input.value === ''){
+          input.style.border = '2px solid #D25555'
+          input.nextElementSibling.innerText = 'Não deixe o formulário em branco.'
+        } else {
+          input.style.border = ''
+          input.nextElementSibling.innerText = ''
+        }
+      })
+    }
+  
+  }
+  addEventClick(){
+    this.btn.addEventListener('click',this.addElement)
+    if(this.deletElements){this.deletElements.forEach((element)=> element.addEventListener('click',this.deletElement))}
+  }
+
+  init(){
+    if(this.wrapper && this.form){this.addEventClick();this.deletEvent()}
+    return this
+  }
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,13 @@
 import Menu from './modules/menu.js'
+import Notes from './modules/notes.js'
 
 const activeClass = 'active'
 
 const menu = new Menu('.header-menu','.ul-nav','.close-menu',activeClass).init()
 const modal = new Menu('#add-note','.modal-note','.modal-close',activeClass,true).init()
 const modal2 = new Menu('.add-btn','.modal-g','.modal-close',activeClass,true).init()
+
+export default modal
+
+const notes = new Notes('.note-wrapper','#note').init()
+

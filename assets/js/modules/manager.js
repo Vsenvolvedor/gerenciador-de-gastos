925a24604a8cf7b1a83ac7d427887c038aba1cbf
Vitor <catchtherainbow33@gmail.com>
2022-04-11 23:28:12 -0300

    Manager atualizaçao de codigo

--- assets/js/modules/manager.js
+++ assets/js/modules/manager.js
@@ -1,59 +1,93 @@
-import {modal2} from "../script.js"
+import {modal, modal2} from "../script.js"
 
-class Manager {
-  constructor(wrapper,form){
-    this.wrapper = document.querySelector(wrapper)
-    this.form = document.querySelector(form)
-    this.inputs = [...this.form].filter((item) => !(item.id === 'btn'))
-    this.btn = [...this.form].filter((item) => item.id === 'btn')[0]
-    this.addElement = this.addElement.bind(this)
+function manager() {
+  
+  const 
+    wrapper = document.querySelector('.g-wrapper'),
+    form = [...document.querySelector('#modal')],
+    itemType = document.querySelectorAll('.type'),
+    formObj = {}
+    form.forEach((item) => {
+      formObj[item.name] = item
+    })
+
+  
+  function checkItem({target},items){
+    items.forEach((item) => item.classList.remove('active'))
+    if(target){
+      target.classList.add('active')
+      formObj.type = target.htmlFor
+    }
+  
   }
-  createElement(){
+
+  function deletElement({target}){
+    target.parentElement.parentElement.parentElement.remove()
+  }
+
+  function addDeletEvent(){
+    const deletElements = document.querySelectorAll('.delet-note')
+    deletElements.forEach(element => element.addEventListener('click',deletElement))
+  }
+
+  function createElement(){
     const element = `
     <div class="note">
-      <div>
-        <h3>
-         ${this.form.name.value}
-        </h3>
-        <button class="delet-note">
-          <img src="assets/images/delete.svg" alt="Deletar">
-        </button>
-      </div>
-      <ul>
-        <li>Valor: R$ ${this.form.price.value} </li>
-        <li>Tipo: ${this.form.type.value}</li>
-        <li>Categoria: ${this.form.categ.value} </li>
-        <li>Pago? ${this.form.check.value} </li>
-      </ul>
+    <div>
+      <h3>
+        ${formObj.name.value}
+      </h3>
+      <button class="delet-note">
+        <img src="assets/images/delete.svg" alt="Deletar">
+      </button>
     </div>
-  `
+    <ul>
+      <li>Valor: R$ <span>${formObj.price.value}</span> </li>
+      <li>Tipo: <span>${formObj.type}</span></li>
+      <li>Categoria: <span>${formObj.categ.value}</span> </li>
+    </ul>
+    </div>
+    `
     return element
   }
-  deletEvent(){
-    this.deletElements = document.querySelectorAll('.delet-note');
-    this.addEventClick();
-  }
-  deletElement({target}){
-    target.parentElement.parentElement.parentElement.remove();
-  }
-  addElement(){
 
-    const element = this.createElement();
-    this.wrapper.innerHTML += element;
-    modal2.toggleMenu()
-    document.querySelector('body').style.overflowY = 'auto'
-    this.deletEvent();
+  function validateInputs(){
+    let checkName = false
+    const valid = form.filter(item => item.name !== 'btn').map((item) => {
+      if(item.name === 'type' && !checkName){
+        checkName = true
+        return true    
+      }
+      else if(!item.value){
+        return !!(item.value)
+      }
+      else{
+        item.remove()
+      }
+    })
+    
+    return valid.every(item => !!item)
   }
+
+  function addElement(){
+    const validate = validateInputs()
+    if(validate){
+      const element = createElement()
+      wrapper.innerHTML += element
+      addDeletEvent()
+      modal2.toggleMenu()
+      document.querySelector('body').style.overflowY = 'auto'
+    
+      
+    } else {
+    console.log(validate)
+      
+    }
+  }
+
+  formObj.btn.addEventListener('click',addElement)
+  itemType.forEach(item => item.addEventListener('click', (event) => {checkItem(event,itemType)}))
   
-  addEventClick(){
-    this.btn.addEventListener('click',this.addElement)
-    if(this.deletElements){this.deletElements.forEach((element)=> element.addEventListener('click',this.deletElement))}
-  }
-
-  init(){
-    if(this.wrapper && this.form){this.addEventClick();this.deletEvent()}
-    return this
-  }
 }
 
-const manager = new Manager('.g-wrapper','#modal').init()
+manager()

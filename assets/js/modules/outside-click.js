27b184b2471289e5d7e9bfc5accce089d77d7d45
Vitor <catchtherainbow33@gmail.com>
2022-04-17 02:23:45 -0300

    Modais responsivos

--- assets/js/modules/outside-click.js
+++ assets/js/modules/outside-click.js
@@ -4,6 +4,7 @@ export default function outsideClick(btn,element,act){
     if(!(element.contains(target) || btn.contains(target))){
       element.classList.remove(act)
       element.parentElement.classList.remove(act)
+      document.querySelector('body').style.overflowY = 'auto'
       window.removeEventListener('click', outside)
     }
   }

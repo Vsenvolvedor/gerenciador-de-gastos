88e7c6da8758be224fcfbca2ac62567c19b253a9
Vitor <catchtherainbow33@gmail.com>
2022-04-20 16:08:17 -0300

    class menu refatorado

--- public/assets/js/modules/menu.js
+++ public/assets/js/modules/menu.js
@@ -1,31 +1,32 @@
 import outsideClick from "./outside-click.js"
 
 export default class Menu{
-  constructor(btn,menu,closeMenu,activeClass,child){
-    this.btn = document.querySelector(btn)
-    this.menu = document.querySelector(menu)
-    this.closeMenu = document.querySelectorAll(closeMenu)
-    this.activeClass = activeClass
-    this.child = child | false
-
-    this.toggleMenu = this.toggleMenu.bind(this)
+  constructor(btn, menu ,closeMenu, activeClass, child) {
+    this.btn = document.querySelector(btn);
+    this.menu = document.querySelector(menu);
+    this.closeMenu = document.querySelectorAll(closeMenu);
+    this.activeClass = activeClass;
+    this.child = child | false;
   }
-  toggleMenu(event, type = false){
-    this.menu.classList.toggle('active')
-    if(this.child){
-      document.querySelector('body').style.overflowY = type || 'hidden'
-      outsideClick(this.btn,this.menu.firstElementChild,this.activeClass)
+  toggleMenu(event, type = false) {
+    this.menu.classList.toggle('active');
+    if (this.child) {
+      document.querySelector('body').style.overflowY = type || 'hidden';
+      outsideClick(this.btn, this.menu.firstElementChild, this.activeClass);
     }
-    else{outsideClick(this.btn,this.menu,this.activeClass)}
+    else outsideClick(this.btn, this.menu, this.activeClass);
   }
-
   addEvent(){
-    this.btn.addEventListener('click',this.toggleMenu)
-    this.closeMenu.forEach(item => item.addEventListener('click',(event) => this.toggleMenu(event,'auto')))
+    this.btn.addEventListener('click', this.toggleMenu);
+    this.closeMenu.forEach(item => item.addEventListener('click', event => this.toggleMenu(event, 'auto')));
+  }
+  setThis() {
+    this.toggleMenu = this.toggleMenu.bind(this);
   }
   init(){
     if(this.btn && this.menu){
-      this.addEvent()
+      this.setThis();
+      this.addEvent();
     }
     return this
   }

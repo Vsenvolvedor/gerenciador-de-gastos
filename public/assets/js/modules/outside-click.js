5e80775ee734123ad26efd4d8913c7fe54b95676
Vitor <catchtherainbow33@gmail.com>
2022-04-20 16:04:41 -0300

    outclick refatorado

--- public/assets/js/modules/outside-click.js
+++ public/assets/js/modules/outside-click.js
@@ -1,13 +1,13 @@
 export default function outsideClick(btn,element,act){
 
-  function outside({target}){
-    if(!(element.contains(target) || btn.contains(target))){
-      element.classList.remove(act)
-      element.parentElement.classList.remove(act)
-      document.querySelector('body').style.overflowY = 'auto'
-      window.removeEventListener('click', outside)
-    }
-  }
+  function outside( { target } ){
+    if( !(element.contains(target) || btn.contains(target) ) ) {
+      element.classList.remove(act);
+      element.parentElement.classList.remove(act);
+      document.querySelector('body').style.overflowY = 'auto';
+      window.removeEventListener('click', outside);
+    };
+  };
 
-  window.addEventListener('click',outside)
+  window.addEventListener('click',outside);
 }
